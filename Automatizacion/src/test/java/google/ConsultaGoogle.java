59007ce238d643d9f6b89b7d9764760fbb8282fa
Javier Jaramillo <javier.jarates@gmail.com>
2018-11-10 11:28:34 -0500

    Se modifica el seteo del chromedriver

--- Automatizacion/src/test/java/google/ConsultaGoogle.java
+++ Automatizacion/src/test/java/google/ConsultaGoogle.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class ConsultaGoogle {
 
 	public static void main(String[] args) throws InterruptedException {
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\javier\\eclipse-workspace\\Automatizacion\\drivers\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("http://www.google.com");
 		driver.findElement(By.id("lst-ib")).sendKeys("Selenium hq");
