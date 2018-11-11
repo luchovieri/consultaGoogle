6dfc595faba930fcbbc805bfc3493781f69f03ec
Javier Jaramillo <javier.jarates@gmail.com>
2018-11-11 12:57:42 -0500

    Elimina código comentado

--- KometSalesProject/src/test/java/prueba/PruebaTecnicaKometSales.java
+++ KometSalesProject/src/test/java/prueba/PruebaTecnicaKometSales.java
@@ -6,6 +6,8 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class PruebaTecnicaKometSales {
 	
@@ -13,13 +15,11 @@ public class PruebaTecnicaKometSales {
 	public void consultarPagina() throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-//		WebDriverWait wait = new WebDriverWait(driver, 10);
-//		WebElement element = wait.until(ExpectedConditions.elementToBeClickable(By.id("lst-ib")));		driver.get("http://www.google.com");
+		WebDriverWait wait = new WebDriverWait(driver, 10);
 		driver.get("https://www.seleniumhq.org");
-		driver.findElement(By.id("menu_download")).click();
+		wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.id("menu_download")))).click();
 		String title= driver.getTitle();
 		assertEquals(title, "Downloads");
-		Thread.sleep(3000);
 		driver.close();
 		driver.quit();
 	}
