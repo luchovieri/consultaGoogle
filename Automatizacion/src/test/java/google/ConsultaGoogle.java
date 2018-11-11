035d3bac85d3c0ed0c443b65afd70e4eaa6701cc
Javier Jaramillo <javier.jarates@gmail.com>
2018-11-11 09:42:00 -0500

    Se sube proyecto base Komet Sales

--- Automatizacion/src/test/java/google/ConsultaGoogle.java
+++ Automatizacion/src/test/java/google/ConsultaGoogle.java
@@ -2,13 +2,18 @@ package google;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class ConsultaGoogle {
 
 	public static void main(String[] args) throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
+//		WebDriverWait wait = new WebDriverWait(driver, 10);
+//		WebElement element = wait.until(ExpectedConditions.elementToBeClickable(By.id("lst-ib")));		driver.get("http://www.google.com");
 		driver.get("http://www.google.com");
 		driver.findElement(By.id("lst-ib")).sendKeys("Selenium hq");
 		driver.findElement(By.name("btnK")).click();
