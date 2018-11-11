e84a195d40ac11acce1a38d875e8056f9dd79d45
Javier Jaramillo <javier.jarates@gmail.com>
2018-11-11 15:29:40 -0500

    Prueba con métodos exitoso y fallido para prueba técnica Komet Sales

--- KometSalesProject/src/test/java/prueba/PruebaTecnicaKometSales.java
+++ KometSalesProject/src/test/java/prueba/PruebaTecnicaKometSales.java
@@ -11,15 +11,38 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class PruebaTecnicaKometSales {
 	
+	public static String resExitoso = "Tracking - AWB Summary";
+	public static String resFallido = "Usuario o contraseña incorrecto";
+	public static String usuario = "gp";
+	public static String claveCorrecta = "demo";
+	public static String claveIncorrecta = "demo2";
+	
 	@Test
-	public void consultarPagina() throws InterruptedException {
+	public void loginExitoso() throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		WebDriverWait wait = new WebDriverWait(driver, 10);
-		driver.get("https://www.seleniumhq.org");
-		wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.id("menu_download")))).click();
-		String title= driver.getTitle();
-		assertEquals(title, "Downloads");
+		driver.get("https://app.kometsales.com ");
+		wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.id("txtUserName")))).sendKeys(usuario);
+		wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.id("txtPassword")))).sendKeys(claveCorrecta);
+		wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//*[@id=\"frmSignin\"]/button")))).click();
+		String resultado = driver.getTitle();
+		assertEquals(resultado, resExitoso);
+		driver.close();
+		driver.quit();
+	}
+	
+	@Test
+	public void loginFallido() throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+		driver.get("https://app.kometsales.com ");
+		wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.id("txtUserName")))).sendKeys(usuario);
+		wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.id("txtPassword")))).sendKeys(claveIncorrecta);
+		wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//*[@id=\"frmSignin\"]/button")))).click();
+		String resultado = wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("/html/body/div[1]/div[1]/div/div/div[1]/div/div[2]/div[2]")))).getText();
+		assertEquals(resultado, resFallido);
 		driver.close();
 		driver.quit();
 	}
